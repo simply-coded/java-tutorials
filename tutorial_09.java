aa6decaeb03c65426b5a3f979e9cb73dcb4593e5
Jeremy England <simplycoded.help@gmail.com>
2017-02-06 17:59:59 -0600

    Create tutorial_09.java

---
+++ tutorial_09.java
@@ -0,0 +1,14 @@
+public class Driver 
+{
+  public static void main(String[] args) 
+  {          
+
+      String fName = "Jeremy";
+      String lName = "England";
+
+      String fullName = fName + " " + lName;
+      String msg1 = "My name is " + fullName;
+      String msg2 = "My last name is " + lName + ", but I have never been there.";
+
+  } 
+}

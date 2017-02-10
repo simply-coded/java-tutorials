9cc29b7fa7eea65f97de311b7dd0c186315e728b
Jeremy England <simplycoded.help@gmail.com>
2017-02-09 21:55:16 -0600

    Create tutorial_13.java

---
+++ tutorial_13.java
@@ -0,0 +1,7 @@
+public class tutorial_13 {
+    public static void main(String[] args) {
+
+        System.out.print( "Hello,\nThe location you are wanting is in:\n\n \t \"C:\\Windows\\Lime\" \n\nHave fun.");     
+
+    }
+}

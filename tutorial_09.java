91273056fa68b45873344bfafc9c378cbc985d82
Jeremy England <simplycoded.help@gmail.com>
2017-02-09 21:56:30 -0600

    Update tutorial_09.java

--- tutorial_09.java
+++ tutorial_09.java
@@ -1,14 +1,12 @@
-public class Driver 
-{
-  public static void main(String[] args) 
-  {          
+class Driver {
+    public static void main(String[] args) {
 
-      String fName = "Jeremy";
-      String lName = "England";
+        String fName = "Jeremy";
+        String lName = "England";
 
-      String fullName = fName + " " + lName;
-      String msg1 = "My name is " + fullName;
-      String msg2 = "My last name is " + lName + ", but I have never been there.";
+        String fullName = fName + " " + lName;
+        String msg1 = "My name is " + fullName;
+        String msg2 = "My last name is " + lName + ", but I have never been there.";
 
-  } 
+    }
 }

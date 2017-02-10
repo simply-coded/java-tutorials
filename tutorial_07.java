c1e78ce87623a0bac396edb811a2caafb64c1392
Jeremy England <simplycoded.help@gmail.com>
2017-02-09 21:59:54 -0600

    Update tutorial_07.java

--- tutorial_07.java
+++ tutorial_07.java
@@ -1,38 +1,45 @@
-class Driver
-{
-  public static void main(String[] args) 
-  {
-    // These all make a reference with the words "Hello !" in them.
-    String msg1 = new String("Hello !");
-    String msg2 = new String(new char[]{'H', 'e', 'l', 'l', 'o', ' ', '!'});
-    String msg3 = "Hello !";
-    
-    System.out.println( msg1 );
-    System.out.println( msg2 );
-    System.out.println( msg3 );
-    
-    // different ways to declare and initialize
-    String a;
-    a = "alfa";
+class Driver {
+    public static void main(String[] args) {
+        
+        // These all make a reference with the words "Hello !" in them.
+        String msg1 = new String("Hello !");
+        String msg2 = new String(new char[] {
+            'H',
+            'e',
+            'l',
+            'l',
+            'o',
+            ' ',
+            '!'
+        });
+        String msg3 = "Hello !";
 
-    String b = "bravo";
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.println(msg3);
 
-    String c, d, e;
-    c = "charlie";
-    d = "delta";
-    e = "echo";
+        // different ways to declare and initialize
+        String a;
+        a = "alfa";
 
-    String f = "foxtrot", g, h = "hotel";
-    g = "golf";
+        String b = "bravo";
 
-    String i;
-    String j;
-    String k;
-    i = "india";
-    j = "juliett";
-    k = "kilo";
-    
-    String l = "lima", m = "mike";
-  
-  }
+        String c, d, e;
+        c = "charlie";
+        d = "delta";
+        e = "echo";
+
+        String f = "foxtrot", g, h = "hotel";
+        g = "golf";
+
+        String i;
+        String j;
+        String k;
+        i = "india";
+        j = "juliett";
+        k = "kilo";
+
+        String l = "lima", m = "mike";
+
+    }
 }

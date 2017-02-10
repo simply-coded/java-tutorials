d8899f068c05001de86bb957722788c688bb96c1
Jeremy England <simplycoded.help@gmail.com>
2017-02-09 21:59:18 -0600

    Update tutorial_06.java

--- tutorial_06.java
+++ tutorial_06.java
@@ -1,21 +1,19 @@
-class Driver 
-{
-  public static void main(String[] args)
-  {
-  
-    double num1 = 65.0;
-    float num2 = 65.0f;
-    
-    long num3 = 65;
-    int num4 = 65;
-    short num5 = 65;
-    byte num6 = 65;
-    
-    boolean isHappy = true;
-    boolean isSad = false;
-           
-    char letterA = 'A';
-    char alternativeLetterA = 65;
-    
-  }
+class Driver {
+    public static void main(String[] args) {
+
+        double num1 = 65.0;
+        float num2 = 65.0 f;
+
+        long num3 = 65;
+        int num4 = 65;
+        short num5 = 65;
+        byte num6 = 65;
+
+        boolean isHappy = true;
+        boolean isSad = false;
+
+        char letterA = 'A';
+        char alternativeLetterA = 65;
+
+    }
 }

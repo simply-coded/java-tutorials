0302ab7eb38fc018af934fa42fbcabde9872fa4f
Jeremy England <simplycoded.help@gmail.com>
2017-03-01 17:43:46 -0600

    Create tutorial_27.java

---
+++ tutorial_27.java
@@ -0,0 +1,30 @@
+/**
+ * Driver
+ */
+class Driver {
+    //
+    public static void main(String[] args) {          
+        
+        System.out.println( greeting() );
+        System.out.println( greeting("Jeremy") );
+        System.out.println( greeting("Jeremy", "England") );
+        
+    } 
+    //
+    static String greeting() {
+        return greeting("Person", "");
+    }
+    //
+    static String greeting(String f) {
+        return greeting(f, "");
+    }
+    // added a ternary operator here as per request to get rid of the extra space when only putting a first name.
+    static String greeting(String f, String l) {        
+        return (
+            "--------------------------------\n" +
+            "Hello, " + f + (l.equals("") ? "": " ") + l + ". Welcome to my app.\n" +
+            "--------------------------------"    
+        );
+    }
+    //
+}
